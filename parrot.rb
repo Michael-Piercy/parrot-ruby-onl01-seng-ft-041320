7b3ebeb7fac5e657b166859496ee9be55fd347a6
Michael Piercy <michael.piercy@zoho.com>
2020-04-14 01:53:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Pretty bird!")
-  puts "Pretty bird!"
-  return "Pretty bird!"
+  puts "phrase #{"Pretty bird!"}"
+  return "phrase #{Pretty bird}"
 end
